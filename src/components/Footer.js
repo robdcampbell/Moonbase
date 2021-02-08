3d435ab639e864005893c09d65868585edfabe9a
robdcampbell <rdeancampbell@gmail.com>
2021-02-08 11:09:55 -0500

    Finish footer styles (for now)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <p>rob campbell 2021</p>
-    </div>
+    <footer className="footer__container">
+      <div className="footer__logo">
+        <h4>moonbase.</h4>
+        <a
+          href="https://github.com/robdcampbell/projectsDashboard-Auth"
+          target="_blank"
+        >
+          click here to view project source code.
+        </a>
+      </div>
+
+      <a href="https://www.robcampbelldev.com" target="_blank">
+        rob campbell. 2021
+      </a>
+    </footer>
   );
 };
 
