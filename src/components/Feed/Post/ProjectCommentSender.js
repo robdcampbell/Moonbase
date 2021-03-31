ab1fda5f1c0e505f8ee814ec8b0cd589ed47ffc3
robdcampbell <rdeancampbell@gmail.com>
2021-03-30 23:48:19 -0400

    Add comment to project funcationality wired up + Firestore Rules updated.

--- src/components/Feed/Post/ProjectCommentSender.js
+++ src/components/Feed/Post/ProjectCommentSender.js
@@ -1,44 +1,58 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { firestore } from "../../../firebase/config";
 import { useParams } from "react-router-dom";
 import firebase from "firebase";
 import AddModal from "../MessageSender/AddModal";
+import { useProjects } from "../../../context/ProjectsContext";
 
 const ProjectCommentSender = () => {
+  const {
+    activeProject,
+    setActiveProject,
+    activeTitleUpdate,
+    setActiveTitleUpdate,
+    activeDescriptionUpdate,
+    setActiveDescriptionUpdate,
+    activeDeadlineUpdate,
+    setActiveDeadlineUpdate,
+    activeStatusUpdate,
+    setActiveStatusUpdate,
+  } = useProjects();
+
   const [projectTitle, setProjectTitle] = useState("");
   const [description, setDescription] = useState("");
   const [projectDeadline, setProjectDeadline] = useState("");
   const [showUpdateModal, setShowUpdateModal] = useState(false);
   const params = useParams();
-  const projectTitleRef = useRef(null);
 
   useEffect(() => {}, []);
 
   const handleSubmit = async () => {
+    console.log(description);
     setShowUpdateModal(false);
-    const docId = new Date().valueOf().toString();
+    const commentId = new Date().valueOf().toString();
     const timeStamp = firebase.firestore.FieldValue.serverTimestamp();
 
-    const projectsRef = firestore
+    const commentsRef = firestore
       .collection("users")
       .doc(params.id)
       .collection("projects")
-      .doc(docId);
+      .doc(activeProject.docId)
+      .collection("comments")
+      .doc(commentId);
 
-    if (description !== "" && projectTitle !== "") {
-      await projectsRef.set({
+    if (description !== "") {
+      await commentsRef.set({
         description: description,
-        title: projectTitle,
-        deadline: projectDeadline,
-        status: "in-progress",
+        title: activeProject.title,
         timeStamp,
-        docId: docId,
+        commentId,
       });
       setDescription("");
       setProjectDeadline("");
       setProjectTitle("");
     } else {
-      alert("Required Fields!");
+      alert("Must add project note!");
     }
   };
 
@@ -54,17 +68,16 @@ const ProjectCommentSender = () => {
                 handleSubmit={handleSubmit}
               />
             )}
-            <h3>Project Update/Comment:</h3>
           </div>
           <div className="input__description ">
-            <label htmlFor="project-description">Project Description *</label>
+            <label htmlFor="project-description">New Project note:</label>
             <textarea
               name="project-description"
               value={description}
               rows="7"
               onChange={(e) => setDescription(e.target.value)}
               type="text"
-              placeholder={`Project description...`}
+              placeholder={`Project update...`}
               className="sender__input"
               required
             />
@@ -76,7 +89,6 @@ const ProjectCommentSender = () => {
           >
             Add Project Note
           </button>
-          <p>*Field is required.</p>
         </form>
       </div>
     </>
